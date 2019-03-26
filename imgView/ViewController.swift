b6d17991e57896b24fd3687851b58e5365df061c
dit08 <dit@imac-3.local>
2019-03-26 15:50:38 +0900

    외계인 춤

--- imgView/ViewController.swift
+++ imgView/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgView: UIImageView!
+    var count = 1
+    var d = 0
+    @IBOutlet weak var lbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imgView.image = UIImage(named: "frame1.png")
+        lbl.text = "1"
     }
-
+    @IBAction func btn(_ sender: Any) {
+        
+        if count >= 1 && d==0{
+            count += 1
+            if count==5{
+                d=1
+            }
+        }else if count<=5&&d==1{
+            count-=1
+            if count==1{
+                d=0
+            }
+        }
+        
+        imgView.image = UIImage(named: "frame\(count).png")
+        
+        lbl.text = String(count)
+    }
+    
+    
 
 }
 
